86141c6462c84699c8c8fbb571130cb8b8534690
nir-peled <nir@peled.cc>
2025-02-27 20:42:09 +0200

    generalised file type a bit

--- src/settings.rs
+++ src/settings.rs
@@ -1,3 +1,9 @@
+/*
+ * Settings is meant to represent program-wide settings read at runtime from
+ * an env-like file, into any types (or, at least, those convertible from string),
+ * in addition to raising errors on duplicate or missing fields.
+ * This is achieved using a macro to build the struct & the reader function
+*/
 use std::fmt;
 use std::fs::File;
 use std::io::{self, BufRead};
@@ -8,6 +14,10 @@ pub enum SettingsReadError {
 	MissingField(String),
 	DuplicateField(String),
 	BadFile(String),
+}
+
+pub enum SettingsFileReadMode {
+	ENV,
 }
 
 impl std::error::Error for SettingsReadError {}
@@ -74,17 +84,22 @@ macro_rules! Settings {
 		impl Settings {
 			pub fn from_env_file<P>(filename: P) -> Result<Self, SettingsReadError>
 			where P: AsRef<Path>, {
+				Self::from_file(filename, SettingsFileReadMode::ENV)
+			}
+
+			pub fn from_file<P>(filename: P, read_mode: SettingsFileReadMode) -> Result<Self, SettingsReadError>
+			where P: AsRef<Path>, {
 				let mut read_value = SettingsReadValues::empty();
 				let lines = Self::read_lines(filename)?;
 
 				for line in lines {
-					let (field_name, field_value) = Self::line_parts(line)?;
+					let (field_name, field_value) = Self::line_parts(line, &read_mode)?;
 					let mut found = false; // to avoid checking fields after found
 					$(
 						if !found && stringify!($field) == field_name {
 							found = true;
 							match read_value.$field {
-								None => read_value.$field = Some(field_value.parse::<$t>()?),
+								None => read_value.$field = Some(field_value.parse()?),
 								Some(_) => return Err(SettingsReadError::DuplicateField(stringify!($field).to_string()))
 							}
 						}
@@ -108,7 +123,13 @@ macro_rules! Settings {
 					Ok(io::BufReader::new(file).lines())
 			}
 
-			fn line_parts(line: Result<String, std::io::Error>) -> Result<(String, String), SettingsReadError> {
+			fn line_parts(line: Result<String, std::io::Error>, mode: &SettingsFileReadMode) -> Result<(String, String), SettingsReadError> {
+				match (mode) {
+					SettingsFileReadMode::ENV => Self::env_line_split(line),
+				}
+			}
+
+			fn env_line_split(line: Result<String, std::io::Error>) -> Result<(String, String), SettingsReadError> {
 				let line_value = line?;
 				let mut splitter = line_value.splitn(2, "=");
 				let error_maker = || SettingsReadError::BadFile("bad line formatting".to_string());
